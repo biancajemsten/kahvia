054612eab92cb48e9a326baf7aa992cc58508784
Bianca Jemsten <biancajemsten@gmail.com>
2020-11-06 22:22:36 +0000

    Added barebone data-dashboard site

---
+++ pages/data-dashboard.js
@@ -0,0 +1,9 @@
+import { Empty } from "antd";
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>This is a company data dashboard</h1>
+      <Empty />
+    </div>
+  );
+}
