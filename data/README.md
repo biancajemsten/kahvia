4fb3d29eb5bebe9d54dc2e9a69a7ea71f3e6542a
unknown <isabelrlewis@gmail.com>
2020-11-06 23:10:53 +0000

    Added delivery schedule and updated data files to use JS so we can pass variables if needed

---
+++ data/README.md
@@ -0,0 +1,13 @@
+## Data files
+
+# cafe.json
+
+Contains data for a singular cafe with orders placed
+
+# deliverySchedule.json
+
+Contains data for the delivery schedule showing nearby deliveries to allow people nearby to book similar time delivery slots
+
+# trends.json
+
+Contains data which maps out key trends we'll be displaying to consumers, to mock the backend call of information including most frequently bought products, seasonal trends, location trends
---
+++ data/deliverySchedule.js
@@ -0,0 +1,31 @@
+const deliverySchedule = (date = "2016-11-16") => ({
+  deliverySchedule: [
+    {
+      date: `${date} 10:30:00.000`,
+      zipCode: "97700",
+      city: "RANUA",
+    },
+    {
+      date: `${date} 11:00:00.000`,
+      zipCode: "97700",
+      city: "RANUA",
+    },
+    {
+      date: `${date} 12:30:00.000`,
+      zipCode: "97700",
+      city: "RANUA",
+    },
+    {
+      date: `${date} 14:45:00.000`,
+      zipCode: "97700",
+      city: "RANUA",
+    },
+    {
+      date: `${date} 16:15:00.000`,
+      zipCode: "97700",
+      city: "RANUA",
+    },
+  ],
+});
+
+export default deliverySchedule;
