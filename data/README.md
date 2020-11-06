e3d1d53a27864d66f1215d1f7751c911751065eb
unknown <isabelrlewis@gmail.com>
2020-11-06 23:43:35 +0000

    Added trending data

--- data/README.md
+++ data/README.md
@@ -12,4 +12,6 @@ Contains data for the delivery schedule showing nearby deliveries to allow peopl
 
 Contains data which maps out key trends we'll be displaying to consumers, to mock the backend call of information including most frequently bought products, seasonal trends, location trends
 
-- months shows sales and quantity for each product
+- months shows sales and quantity for each product from a product point of view how much has been sold in that location or season
+  - using rand values so if you compare on the same screen two different towns or towns and cities then it would be different
+- location shows popular products for each location from a location point of view
--- data/trends.js
+++ data/trends.js
@@ -1,20 +1,63 @@
 const trends = () => ({
-  months: {
-    January: rand(),
-    February: rand(),
-    March: rand(),
-    April: rand(),
-    May: rand(),
-    June: rand(),
-    July: rand(),
-    August: rand(),
-    September: rand(),
-    October: rand(),
-    November: rand(),
-    December: rand(),
+  productTrend: {
+    seasonal: {
+      months: {
+        January: rand(0, 3),
+        February: rand(2, 8),
+        March: rand(1, 10),
+        April: rand(4, 6),
+        May: rand(4, 7),
+        June: rand(6, 9),
+        July: rand(6, 10),
+        August: rand(8, 10),
+        September: rand(6, 9),
+        October: rand(3, 9),
+        November: rand(3, 6),
+        December: rand(7, 10),
+      },
+      town: {
+        January: rand(0, 3),
+        February: rand(2, 8),
+        March: rand(1, 10),
+        April: rand(4, 6),
+        May: rand(4, 7),
+        June: rand(6, 9),
+        July: rand(6, 10),
+        August: rand(8, 10),
+        September: rand(6, 9),
+        October: rand(3, 9),
+        November: rand(3, 6),
+        December: rand(7, 10),
+      },
+    },
+  },
+
+  locationTrend: {
+    city: {
+      productCode: "31",
+      productName: "Vendor Whitener",
+      productSubgroup: "Vending Powders",
+      quantity: 1,
+    },
+    country: {
+      productCode: "56",
+      productName: "Tazza Low Lactose Hot Choco",
+      productSubgroup: "Vending Powders",
+      quantity: 1,
+    },
+    zipCode: {
+      productCode: "31",
+      productName: "Vendor Whitener",
+      productSubgroup: "Vending Powders",
+      quantity: 1,
+    },
   },
 });
 
-const rand = () => Math.floor(Math.random() * 10);
+const rand = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 export default trends;
