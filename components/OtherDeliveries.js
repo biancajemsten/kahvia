9ec0d3ed7f427ff05102718ddb73cebaed99e676
Ashleigh Hyslop <ashleighhyslop@Ashleighs-MBP.broadband>
2020-11-07 20:19:52 +0000

    add other deliveries

---
+++ components/OtherDeliveries.js
@@ -0,0 +1,46 @@
+import { Typography, Row, Col } from "antd";
+import deliverySchedule from '../data/deliverySchedule'
+import { Button } from 'antd';
+const { Text } = Typography;
+import './OtherDeliveries.scss';
+
+export function OtherDeliveries({
+    date,
+    chosenTime
+}) {
+    const deliveries = deliverySchedule(date).deliverySchedule
+    if(chosenTime)
+    return(
+        deliveries.map((item) =>  item.time === chosenTime && (
+     
+            <div className="c-other-deliveries">
+            <Row>
+                <Col span={7}>
+               <Row><Text>{item.customerName}</Text></Row> 
+               <Row> <Text>{item.date}</Text></Row>
+                </Col>
+                <Col span={8}>
+                <Button type="primary">Join this delivery</Button>
+                </Col>
+                <Col span={9}>
+                <Button type="primary">Stay with my own delivery</Button>
+                </Col>
+            </Row>
+        </div> 
+        ))
+    );
+    else
+    return(
+     deliveries.map((item) => (
+     
+            <div className="c-other-deliveries">
+            <Row className="c-other-deliveries__row">
+                <Text>{item.customerName}</Text>
+            </Row>
+            <Row class="c-other-deliveries__row">
+                <Text>{item.date}</Text>
+            </Row>
+        </div> 
+     )      
+ ));      
+}
--- data/deliverySchedule.js
+++ data/deliverySchedule.js
@@ -1,27 +1,37 @@
 const deliverySchedule = (date = "2016-11-16") => ({
   deliverySchedule: [
     {
-      date: `${date} 10:30:00.000`,
+      customerName: "Cafe1",
+      date: `${date}`,
+      time: '10:30:00',
       zipCode: "97700",
       city: "RANUA",
     },
     {
-      date: `${date} 11:00:00.000`,
+      customerName: "Cafe2",
+      date: `${date}`,
+      time: "11:00:00",
       zipCode: "97700",
       city: "RANUA",
     },
     {
-      date: `${date} 12:30:00.000`,
+      customerName: "Cafe3",
+      date: `${date}`,
+      time: '12:30:00',
       zipCode: "97700",
       city: "RANUA",
     },
     {
-      date: `${date} 14:45:00.000`,
+      customerName: "Cafe4",
+      date: `${date}`,
+      time: '14:45:00',
       zipCode: "97700",
       city: "RANUA",
     },
     {
-      date: `${date} 16:15:00.000`,
+      customerName: "Cafe5",
+      date: `${date}`,
+      time: '16:15:00',
       zipCode: "97700",
       city: "RANUA",
     },
