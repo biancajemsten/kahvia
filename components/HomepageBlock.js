a50234c58434465cd6ab118d08cc238e44b1a5c0
Ashleigh Hyslop <ashleighhyslop@Ashleighs-MBP.broadband>
2020-11-07 11:53:01 +0000

    homepage blocks

--- components/HomepageBlock.js
+++ components/HomepageBlock.js
@@ -1,12 +1,39 @@
-import { Typography } from "antd";
-import './Hero.scss';
+import { Typography, Row, Col } from "antd";
 const { Title } = Typography;
+import './HomepageBlock.scss';
+import DeliveryTruck from "../assets/delivery-truck.svg";
+import Insights from "../assets/stats.svg";
+import Networking from "../assets/networking.svg"
 
-export function HomgpageBlock() {
+export function HomepageBlock() {
   return (
-    <div className="c-hero" style={{backgroundImage: "url(" + `${require("../assets/coffee-background.jpg")}` + ")",}}>
-
-
+    <div className="c-blocks">
+    <Row className="c-blocks__row">
+      <Col span={8} align="center">
+      <a href="/">
+        <div className="c-blocks__imageContainer">
+            <img className="c-blocks__image" src={DeliveryTruck} />
+        </div>
+        <Title className="c-blocks__text">Next scheduled delivery</Title>
+      </a>
+      </Col>
+      <Col span={8} align="center">
+      <a href="/data-dashboard">
+        <div className="c-blocks__imageContainer">
+            <img className="c-blocks__image" src={Insights} />
+        </div>
+        <Title className="c-blocks__text">Insights and Market Trends</Title>
+      </a>
+      </Col>
+      <Col span={8} align="center">
+      <a href="/">
+        <div className="c-blocks__imageContainer">
+            <img className="c-blocks__image" src={Networking} />
+        </div>
+        <Title className="c-blocks__text">Network with Suppliers</Title>
+       </a>
+      </Col>
+    </Row>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { Hero } from "../components/Hero";
-
+import {HomepageBlock} from "../components/HomepageBlock";
 export default function Home() {
   return (
     <div>
     <Hero/>
+    <HomepageBlock/>
     </div>
   );
 }
