fad58a4d0e420b40ed13f14d05f3a15e9d802d91
Bianca Jemsten <biancajemsten@gmail.com>
2020-11-07 08:53:17 +0000

    Fixed error

--- components/Loading.js
+++ components/Loading.js
@@ -4,14 +4,14 @@ import { Typography } from "antd";
 
 export function Loading() {
   return (
-    <div class="c-loading__wrapper">
+    <div className="c-loading__wrapper">
       <Typography.Text>
         Hold on, we're brewing up some fresh data
       </Typography.Text>
-      <div class="c-loading__bean-container">
-        <img class="c-loading__bean" src={CoffeBeanURL} />
-        <img class="c-loading__bean" src={CoffeBeanURL} />
-        <img class="c-loading__bean" src={CoffeBeanURL} />
+      <div className="c-loading__bean-container">
+        <img className="c-loading__bean" src={CoffeBeanURL} />
+        <img className="c-loading__bean" src={CoffeBeanURL} />
+        <img className="c-loading__bean" src={CoffeBeanURL} />
       </div>
     </div>
   );
