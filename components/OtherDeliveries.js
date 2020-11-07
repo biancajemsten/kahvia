cea312b625d76f2bb88a13d88b2be7a13c98fe8d
Ashleigh Hyslop <ashleighhyslop@Ashleighs-MBP.broadband>
2020-11-07 23:29:48 +0000

    styling delivery scheduler

--- components/OtherDeliveries.js
+++ components/OtherDeliveries.js
@@ -4,26 +4,28 @@ import { Button } from 'antd';
 const { Text } = Typography;
 import './OtherDeliveries.scss';
 
+
 export function OtherDeliveries({
     date,
     chosenTime
 }) {
     const deliveries = deliverySchedule(date).deliverySchedule
+    
     if(chosenTime)
     return(
         deliveries.map((item) =>  item.time === chosenTime && (
      
             <div className="c-other-deliveries">
-            <Row>
+            <Row className="c-other-deliveries__row">
                 <Col span={7}>
-               <Row><Text>{item.customerName}</Text></Row> 
+               <Row><Text>{item.customerName}, {item.city}</Text></Row> 
                <Row> <Text>{item.date}</Text></Row>
                <Row> <Text>{item.time}</Text></Row>
                 </Col>
-                <Col span={8}>
+                <Col offset={1} span={6}>
                 <Button type="primary">Join this delivery</Button>
                 </Col>
-                <Col span={9}>
+                <Col offset={1}span={7}>
                 <Button type="primary">Stay with my own delivery</Button>
                 </Col>
             </Row>
@@ -35,14 +37,18 @@ export function OtherDeliveries({
      deliveries.map((item) => (
      
             <div className="c-other-deliveries">
-            <Row className="c-other-deliveries__row">
-                <Text>{item.customerName}</Text>
-            </Row>
-            <Row className="c-other-deliveries__row">
-                <Text>{item.date}</Text>
-            </Row>
-            <Row className="c-other-deliveries__row">
-                <Text>{item.time}</Text>
+          <Row className="c-other-deliveries__row">
+                <Col span={7}>
+               <Row><Text>{item.customerName}, {item.city}</Text></Row> 
+               <Row> <Text>{item.date}</Text></Row>
+               <Row> <Text>{item.time}</Text></Row>
+                </Col>
+                <Col offset={1} span={6}>
+                <Button type="primary">Join this delivery</Button>
+                </Col>
+                <Col offset={1}span={7}>
+                <Button type="primary">Stay with my own delivery</Button>
+                </Col>
             </Row>
         </div> 
      )      
