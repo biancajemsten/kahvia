aa79e1ee2f46cdf2dce11ada6faaf3dc90078c9c
Ashleigh Hyslop <ashleighhyslop@Ashleighs-MBP.broadband>
2020-11-07 21:53:34 +0000

    google maps icons

--- components/GoogleMaps.js
+++ components/GoogleMaps.js
@@ -5,8 +5,12 @@ import {
   GoogleMap,
   Marker
 } from "react-google-maps";
+import CoffeBeanURL from "../assets/coffee-bean.svg";
 
-const defaultCenter = { lat: 60.1699, lng:24.9384 };
+const defaultCenter = { lat: 60.1699, lng:24.9384 }
+const center2 = { lat: 60.1789, lng:24.9381 };
+const center3 = { lat: 60.1721, lng:24.9401 };
+const center4 = { lat: 60.1721, lng:24.9281 };
 
 const defaultOptions = { scrollwheel: false };
 
@@ -17,7 +21,10 @@ const RegularMap = withScriptjs(
       defaultCenter={ defaultCenter }
       defaultOptions={ defaultOptions }
     >
-      <Marker position={ defaultCenter } />
+      <Marker position={ defaultCenter } icon={{url:CoffeBeanURL, scaledSize: new google.maps.Size(20, 20), origin: new google.maps.Point(0,0), anchor: new google.maps.Point(0, 0)}} />
+      <Marker position={ center2 } icon={{url:CoffeBeanURL, scaledSize: new google.maps.Size(20, 20), origin: new google.maps.Point(0,0), anchor: new google.maps.Point(0, 0)}} />
+      <Marker position={ center3 } icon={{url:CoffeBeanURL, scaledSize: new google.maps.Size(20, 20), origin: new google.maps.Point(0,0), anchor: new google.maps.Point(0, 0)}} />
+      <Marker position={ center4 } icon={{url:CoffeBeanURL, scaledSize: new google.maps.Size(20, 20), origin: new google.maps.Point(0,0), anchor: new google.maps.Point(0, 0)}} />
     </GoogleMap>
   ))
 );
@@ -29,7 +36,7 @@ const mapElementStyle = { height: '100%' };
 export function GoogleMaps(){
   return (
     <RegularMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key="
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key"
       loadingElement={<div style={ loadingElementStyle } />}
       containerElement={<div style={ containerElementStyle } />}
       mapElement={<div style={ mapElementStyle } />}
